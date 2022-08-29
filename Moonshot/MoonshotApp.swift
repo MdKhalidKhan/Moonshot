7871ea1b20c42a327d5ddc40c7858f45009f83e2
Raj <rajkhulna@outlook.com>
2022-08-29 23:32:07 +0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Khalid Khan on 29/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
